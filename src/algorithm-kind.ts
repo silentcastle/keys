1ca4528e22d4d5122aac5b63f3d05ab869f2057b
Anonymous <dd9efe7fb5c01@protonmail.com>
2020-11-27 10:56:04 +0400

    feat: algorithm kind from string

--- src/algorithm-kind.ts
+++ src/algorithm-kind.ts
@@ -1,4 +1,20 @@
+import { InvalidKeyKindError } from "./invalid-key-kind.error";
+
 export enum AlgorithmKind {
   secp256k1 = "secp256k1",
   ed25519 = "ed25519",
+}
+
+export namespace AlgorithmKind {
+  export function fromString(s: string) {
+    const maybe = s as AlgorithmKind;
+    switch (maybe) {
+      case AlgorithmKind.secp256k1:
+        return AlgorithmKind.secp256k1;
+      case AlgorithmKind.ed25519:
+        return AlgorithmKind.ed25519;
+      default:
+        throw new InvalidKeyKindError(maybe);
+    }
+  }
 }
