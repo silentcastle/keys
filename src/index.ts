1601c472376030fffdbba06f05364ee785299e84
Anonymous <dd9efe7fb5c01@protonmail.com>
2020-11-27 11:49:10 +0400

    chore: expose algorithms as namespace

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ export * from "./public-key";
 export * from "./private-key";
 export * from "./fingerprint";
 
-import * as secp256k1 from "./algorithms/secp256k1";
-import * as ed25519 from "./algorithms/ed25519";
+import * as secp256k1Import from "./algorithms/secp256k1";
+import * as ed25519Import from "./algorithms/ed25519";
 
-export const algorithms = { secp256k1, ed25519 };
+export namespace algorithms {
+  export const secp256k1 = secp256k1Import;
+  export const ed25519 = ed25519Import;
+}
